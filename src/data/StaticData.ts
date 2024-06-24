cadbffc6c905837fb19357f374dcb1a99a48254e
Ernes Filipovic <ernes100@hotmail.com>
2024-06-24 21:53:04 +0200

    refactor: Remove unused image imports and update skillData in StaticData.ts

--- src/data/StaticData.ts
+++ src/data/StaticData.ts
@@ -1,80 +1,53 @@
-import Html from "@assets/HTML.png";
-import Js from "@/assets/JSlogo.png";
-import Css2 from "@/assets/CSS.png";
-import Net from "@/assets/Net.png";
-import Git from "@/assets/Git.png";
-import TS from "@/assets/TS.png";
-import Devops from "@/assets/Devops.png";
-import React from "@/assets/React.png";
-import Postgres from "@/assets/postgres.png";
-import Me from "@/assets/Me.png";
-import Mongo from "@/assets/Mongo.png";
-
 import { skillProps } from "@/sections/Skills";
 import { AssignmentProps } from "@/components/Assignment";
 
-export const Images = {
-  Html,
-  Js,
-  Css2,
-  Net,
-  Git,
-  TS,
-  Devops,
-  React,
-  Postgres,
-  Me,
-  Mongo,
-};
-
 export const skillData: skillProps[] = [
   {
-    imgPath: Images.Net,
+    imgPath: "",
     skillName: ".Net",
     percentage: 60,
   },
   {
-    imgPath: Images.Js,
+    imgPath: "",
     skillName: "JavaScript",
     percentage: 80,
   },
   {
-    imgPath: Images.Html,
     skillName: "Html",
     percentage: 90,
   },
   {
-    imgPath: Images.Css2,
+    imgPath: "",
     skillName: "CSS",
     percentage: 80,
   },
   {
-    imgPath: Images.Git,
+    imgPath: "",
     skillName: "Git",
     percentage: 80,
   },
   {
-    imgPath: Images.TS,
+    imgPath: "",
     skillName: "TypeScript",
     percentage: 80,
   },
   {
-    imgPath: Images.Devops,
+    imgPath: "",
     skillName: "Devops",
     percentage: 70,
   },
   {
-    imgPath: Images.React,
+    imgPath: "",
     skillName: "React",
     percentage: 80,
   },
   {
-    imgPath: Images.Postgres,
+    imgPath: "",
     skillName: "Postgres",
     percentage: 40,
   },
   {
-    imgPath: Images.Mongo,
+    imgPath: "",
     skillName: "Mongodb",
     percentage: 30,
   },
