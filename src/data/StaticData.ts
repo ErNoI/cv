28f9b17f0a3035b4bd13de8559c7450ebd0603c0
Ernes Filipovic <ernes100@hotmail.com>
2024-06-24 21:51:33 +0200

    refactor: Update import paths for assets and components

--- src/data/StaticData.ts
+++ src/data/StaticData.ts
@@ -1,17 +1,17 @@
-import Html from '@assets/HTML.png';
-import Js from '@/assets/JSlogo.png';
-import Css2 from '@/assets/CSS.png';
-import Net from '@/assets/Net.png';
-import Git from '@/assets/Git.png';
-import TS from '@/assets/TS.png';
-import Devops from '@/assets/Devops.png';
-import React from '@/assets/React.png';
-import Postgres from '@/assets/postgres.png';
-import Me from '@/assets/Me.png';
-import Mongo from '@/assets/Mongo.png';
+import Html from "@assets/HTML.png";
+import Js from "@/assets/JSlogo.png";
+import Css2 from "@/assets/CSS.png";
+import Net from "@/assets/Net.png";
+import Git from "@/assets/Git.png";
+import TS from "@/assets/TS.png";
+import Devops from "@/assets/Devops.png";
+import React from "@/assets/React.png";
+import Postgres from "@/assets/postgres.png";
+import Me from "@/assets/Me.png";
+import Mongo from "@/assets/Mongo.png";
 
-import { skillProps } from '@/sections/Skills';
-import { AssignmentProps } from '@/components/Assignment';
+import { skillProps } from "@/sections/Skills";
+import { AssignmentProps } from "@/components/Assignment";
 
 export const Images = {
   Html,
@@ -24,8 +24,8 @@ export const Images = {
   React,
   Postgres,
   Me,
-  Mongo
-}
+  Mongo,
+};
 
 export const skillData: skillProps[] = [
   {
@@ -47,17 +47,17 @@ export const skillData: skillProps[] = [
     imgPath: Images.Css2,
     skillName: "CSS",
     percentage: 80,
-  },  
+  },
   {
     imgPath: Images.Git,
     skillName: "Git",
     percentage: 80,
-  },  
+  },
   {
     imgPath: Images.TS,
     skillName: "TypeScript",
     percentage: 80,
-  },  
+  },
   {
     imgPath: Images.Devops,
     skillName: "Devops",
@@ -72,7 +72,7 @@ export const skillData: skillProps[] = [
     imgPath: Images.Postgres,
     skillName: "Postgres",
     percentage: 40,
-  },  
+  },
   {
     imgPath: Images.Mongo,
     skillName: "Mongodb",
@@ -98,7 +98,6 @@ export const skillData: skillProps[] = [
     skillName: "Microservices",
     percentage: 70,
   },
-  
 ];
 
 export const assignmentData: AssignmentProps[] = [
@@ -106,7 +105,8 @@ export const assignmentData: AssignmentProps[] = [
     company: "Tromb",
     role: "Fullstack Developer / Consultant Volvo",
     description:
-"At VGCS I joined a new team that was assigned the development of a new microservice to address material related challenges within a quarry. The core focus of this assignment was to create a microservice that would provide solutions to the material problems faced by the quarry. These material challenges might include optimizing material extraction, tracking material inventory, monitoring material quality, and ensuring efficient logistics within the quarry. Technical keywords: React, Nest.Js, Prisma, GraphQL, PostgresQL, Docker, Devops, Microservice",    year: "2024",
+      "At VGCS I joined a new team that was assigned the development of a new microservice to address material related challenges within a quarry. The core focus of this assignment was to create a microservice that would provide solutions to the material problems faced by the quarry. These material challenges might include optimizing material extraction, tracking material inventory, monitoring material quality, and ensuring efficient logistics within the quarry. Technical keywords: React, Nest.Js, Prisma, GraphQL, PostgresQL, Docker, Devops, Microservice",
+    year: "2024",
   },
   {
     company: "Friday",
@@ -118,29 +118,29 @@ export const assignmentData: AssignmentProps[] = [
   {
     company: "Toyota Material Handling",
     role: "Backend Developer",
-    description:"At Toyota I worked in the telematics department to further develop Toyotas existing systems. The system was a way for customers to improve their warehouse in every possible way. Gathered a lot of data from the forklifts to provide the customer with statistics which offered an easy way to find improvements. During the time at Toyota a migration was started, the goal of the migration was to turn the current system into cloud-based microservice architecture. This work also included feature flags so the customers were able to work undisturbed without getting interrupted with the new releases. The teams were involved in the whole flow from building the new system parallel to maintaining the old system with responsibility of tasks working with both new development and testing.",
-        year: "2019",
+    description:
+      "At Toyota I worked in the telematics department to further develop Toyotas existing systems. The system was a way for customers to improve their warehouse in every possible way. Gathered a lot of data from the forklifts to provide the customer with statistics which offered an easy way to find improvements. During the time at Toyota a migration was started, the goal of the migration was to turn the current system into cloud-based microservice architecture. This work also included feature flags so the customers were able to work undisturbed without getting interrupted with the new releases. The teams were involved in the whole flow from building the new system parallel to maintaining the old system with responsibility of tasks working with both new development and testing.",
+    year: "2019",
   },
   {
     company: "Visit-group",
     role: "Internship",
     description:
-      "Lorem Ipsum is simplhe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ed. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "At Visit Group, I worked as an Intern with mainly focus on front-end development. The team I was assigned to worked closely with customers to offer a unique website. This was done using a CMS system called drupal.",
     year: "2018",
   },
   {
     company: ".Net",
     role: "Student / Lexicon Gothenburg",
     description:
-      "Lorem Ipsum is simplhe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ed. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "The education covered everything from C# to the .NET framework. We focused a lot on test-driven development, learning how to write tests to guide our coding. Agile methods were a big part of the course too, helping us understand effective project management. I also got hands-on experience with SQL for working with databases.",
     year: "2016",
   },
   {
     company: "Game-development",
     role: "Student / Örebros University",
     description:
-      "Lorem Ipsum is simplhe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ed. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Included various programming languages such as C, C++, C#, Python, and Java, as well as some database technology. In addition to this, I got to use Unity as a game engine and worked with models in Blender. I have also animated and created textures in Photoshop.",
     year: "2013",
   },
-  
 ];
